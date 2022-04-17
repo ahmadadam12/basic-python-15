83c4fc6627f80bf5788517bc89024162d2cc6627
Ahmadadam12 <adam.zam>
2022-04-17 20:48:09 +0700

    able to run

--- exercise14_List_Remove Duplicates.py
+++ exercise14_List_Remove Duplicates.py
@@ -1,22 +1,35 @@
 """
-num1 = int(input("insert a number: "))
-num2 = int(input("insert a number: "))
-a = []
-b = []
-noDuplicates = set()
+Write a program (function!) that takes a list and returns 
+a new list that contains all the elements of the first list minus all the duplicates.
+"""
+
+import random
+
+print(f"this program is for removing duplicates in two randomly generated lists.")
+num1 = random.randint(int(input("min value list1: ")), int(input("max value list1: ")))
+num2 = random.randint(int(input("min value list2: ")), int(input("max value list2: ")))
+
+print(num1)
+print(num2)
+
+a = set()
+b = set()
 
 for i in range(0, num1):
-    a.append(i)
-    noDuplicates.add(a[i])
-    i = i+1
+    a.add(i)
 print(f"num1 list: {a}")
 
 for j in range(0, num2):
-    b.append(j)
-    noDuplicates.add(b[j]) 
-    j = j+1
+    b.add(j)
 print(f"num2 list: {b}")
 
+if len(a) > len(b): 
+    z = a.difference(b)
+    print(z)
+elif len(a) < len(b):
+    z = b.difference(a)
+    print(z)
+elif len(a) == len(b):
+    z = b.difference(a)
+    print(f"your two lists are the same content.")
 
-print(noDuplicates)
-"""
--- first_program.py
+++ first_program.py
@@ -1,20 +1,3 @@
-num1 = int(input("insert a number: "))
-num2 = int(input("insert a number: "))
-a = []
-b = []
-noDuplicates = set()
+thisset = {"apple", "banana", "cherry", "apple"}
 
-for i in range(0, num1):
-    a.append(i)
-    noDuplicates.add(a[i])
-    i = i+1
-print(f"num1 list: {a}")
-
-for j in range(0, num2):
-    b.append(j)
-    noDuplicates.add(b[j]) 
-    j = j+1
-print(f"num2 list: {b}")
-
-
-print(noDuplicates)
+print(thisset)
