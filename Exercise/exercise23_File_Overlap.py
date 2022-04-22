5f11c23f492030dcf2d5e6ca2138ce713fd6de0c
Ahmadadam12 <adam.zam>
2022-04-23 01:08:21 +0700

    solved

--- Exercise/exercise23_File_Overlap.py
+++ Exercise/exercise23_File_Overlap.py
@@ -1,4 +1,37 @@
-with open('happynumbers.txt') as happy:
-    happy_text = happy.readline(3)
+with open(r'C:\Users\adamz\basic-python-15\Exercise\happynumbers.txt') as happy:
+    happy_text = happy.readline()
+    #print(happy_text)
+    happy_list = list()
+    i = 1
+    while happy_text:
+        #print("Line {}: {}".format(i, happy_text.strip()))
+        happy_list.append(happy_text)
+        #print(happy_list)
+        happy_text = happy.readline()
+        #print(happy_text)
+        i += 1
+    #print(happy_list)
 
-print(happy_text)
+with open(r'C:\Users\adamz\basic-python-15\Exercise\primenumbers.txt') as prime:
+    prime_text = prime.readline()
+    #print(happy_text)
+    prime_list = list()
+    i = 1
+    while prime_text:
+        #print("Line {}: {}".format(i, prime_text.strip()))
+        prime_list.append(prime_text)
+        #print(prime_list)
+        prime_text = prime.readline()
+        #print(prime_text)
+        i += 1
+    #print(prime_list)
+
+a = list()
+for x in happy_list:
+    if x in happy_list:
+        if x in prime_list:
+            a.append(x)
+        else:
+            continue
+
+print(a)
