498b64c0654fff12e8bdbc4bd521344e0132a792
Ahmadadam12 <adam.zam>
2022-04-24 11:48:21 +0700

    pick word 1

---
+++ Exercise/exercise25_Pick_a_word.py
@@ -0,0 +1,18 @@
+"""
+In this exercise, the task is to write a function that picks 
+a random word from a list of words from the SOWPODS dictionary. 
+Download this file and save it in the same directory as your Python code. 
+This file is Peter Norvig’s compilation of the dictionary of words used 
+in professional Scrabble tournaments. 
+Each line in the file contains a single word
+
+"""
+
+import random
+
+with open (r'C:\Users\adamz\basic-python-15\Exercise\sowpods.txt') as words:
+    sowpods = words.readlines()
+
+pick_up = random.choice(sowpods)
+
+print(pick_up)
